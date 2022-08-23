1f4712b262d4b44b172eadab50cc4a069581d0bb
zahrul68 <111722318+zahrul68@users.noreply.github.com>
2022-08-23 08:49:10 +0700

    Add files via upload

---
+++ js 1/pernyataan bersyarat/persyaratan/script.js
@@ -0,0 +1,12 @@
+const angka = 30;
+		if(angka > 15 && angka < 50){
+			document.writeln("Angka lebih dari 2 digit");
+		}
+
+		const angko = 50;
+		if (angko < 40 || angko > 30) {
+			document.writeln("Angka lebih dari 2 digit");
+		}
+
+		// latihan
+		const age = 24;
